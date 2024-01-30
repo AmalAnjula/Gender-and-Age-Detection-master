3181afe52c3fac9ee7ba882ae3987a6bcacd730d
AmalAnjula <amalanjula@gmail.com>
2024-01-30 21:02:12 +0000

    Initial commit

---
+++ addrun.py
@@ -0,0 +1,47 @@
+import os
+import tkinter as tk
+from PIL import Image, ImageTk
+
+class SlideshowApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title("Slideshow")
+        self.attributes("-fullscreen", True)  # Open in full-screen mode
+        self.current_slide = tk.Label(self)
+        self.current_slide.pack(expand=True, fill="both")
+        self.first_character = self.read_first_character('first_characters.txt')  # Replace with the actual path to your character file
+        self.image_paths = self.load_images(r'C:\Users\Amal\Downloads\Gender-and-Age-Detection-master', self.first_character)  # Replace with the actual path to your image folder
+        self.current_image_index = 0
+        self.max_images = 5  # Set the maximum number of images to display
+        self.image_counter = 0  # Counter for displayed images
+        self.display_image()
+
+    def read_first_character(self, file_path):
+        with open(file_path, 'r') as file:
+            return file.read(1)
+
+    def load_images(self, folder_path, first_character):
+        image_files = [f for f in os.listdir(folder_path) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')) and f.startswith(first_character)]
+        return [os.path.join(folder_path, image_file) for image_file in image_files]
+
+    def display_image(self):
+        if self.image_counter < self.max_images:
+            image_path = self.image_paths[self.current_image_index]
+            img = Image.open(image_path)
+            img = img.resize((self.winfo_screenwidth(), self.winfo_screenheight()), Image.ANTIALIAS)
+            photo = ImageTk.PhotoImage(img)
+            self.current_slide.config(image=photo)
+            self.current_slide.image = photo
+            self.after(1000, self.next_image)
+            self.image_counter += 1
+        else:
+            # After displaying the specified number of images, close the application
+            self.destroy()
+
+    def next_image(self):
+        self.current_image_index = (self.current_image_index + 1) % len(self.image_paths)
+        self.display_image()
+
+if __name__ == "__main__":
+    app = SlideshowApp()
+    app.mainloop()
---
+++ detect.py
@@ -0,0 +1,130 @@
+#A Gender and Age Detection program by Mahesh Sawant
+
+import cv2
+import math
+import argparse
+import threading
+import time
+import numpy as np
+from scipy.stats import mode
+counter=0
+
+global v_thread
+v_thread=1
+
+array=[]
+
+def highlightFace(net, frame, conf_threshold=0.7):
+    frameOpencvDnn=frame.copy()
+    frameHeight=frameOpencvDnn.shape[0]
+    frameWidth=frameOpencvDnn.shape[1]
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections=net.forward()
+    faceBoxes=[]
+    for i in range(detections.shape[2]):
+        confidence=detections[0,0,i,2]
+        if confidence>conf_threshold:
+            x1=int(detections[0,0,i,3]*frameWidth)
+            y1=int(detections[0,0,i,4]*frameHeight)
+            x2=int(detections[0,0,i,5]*frameWidth)
+            y2=int(detections[0,0,i,6]*frameHeight)
+            faceBoxes.append([x1,y1,x2,y2])
+            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn,faceBoxes
+
+def worker_function():
+    global counter,v_thread
+    """Function to be executed in a virtual thread."""
+    while(v_thread):
+        counter=counter+1
+        time.sleep(1)
+        print(v_thread)
+        
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--image')
+
+args=parser.parse_args()
+
+faceProto="opencv_face_detector.pbtxt"
+faceModel="opencv_face_detector_uint8.pb"
+ageProto="age_deploy.prototxt"
+ageModel="age_net.caffemodel"
+genderProto="gender_deploy.prototxt"
+genderModel="gender_net.caffemodel"
+
+MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList=['Male','Female']
+
+faceNet=cv2.dnn.readNet(faceModel,faceProto)
+ageNet=cv2.dnn.readNet(ageModel,ageProto)
+genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+video=cv2.VideoCapture(args.image if args.image else 0)
+padding=20
+
+
+# Create a virtual thread
+virtual_thread = threading.Thread(target=worker_function)
+
+# Start the virtual thread
+#virtual_thread.start()
+
+old_time=time.time()
+while cv2.waitKey(1)<0 :
+
+
+    nowTime=time.time()
+    if(nowTime-old_time>1):
+        old_time= ( time.time())
+        counter=counter+1
+        #print(counter)
+        if(counter>5):
+            counter=0
+            print (np.unique( array))
+            array.clear()
+         
+
+    
+    hasFrame,frame=video.read()
+    if not hasFrame:
+        cv2.waitKey()
+        break
+    #cv2.imshow("img", frame)
+
+    resultImg,faceBoxes=highlightFace(faceNet,frame)
+    if not faceBoxes:
+        print("No face detected")
+        counter=0
+        array.clear()
+         
+
+    for faceBox in faceBoxes:
+        face=frame[max(0,faceBox[1]-padding):
+                   min(faceBox[3]+padding,frame.shape[0]-1),max(0,faceBox[0]-padding)
+                   :min(faceBox[2]+padding, frame.shape[1]-1)]
+
+        blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds=genderNet.forward()
+        gender=genderList[genderPreds[0].argmax()]
+        #print(f'Gender: {gender}')
+
+        ageNet.setInput(blob)
+        agePreds=ageNet.forward()
+        age=ageList[agePreds[0].argmax()]
+        #print(f'Age: {age[1:-1]} years')
+        #print(counter)
+
+        cv2.putText(resultImg, "Counter "+str(counter),(10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
+        
+        cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
+        cv2.imshow("Detecting age and gender", resultImg)
+        array.append( f'{gender}, {age}')
+
+
+# Wait for the virtual thread to complete (optional)
+v_thread=0
